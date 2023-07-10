9b4dc0236471cefa55eff71b6c063f7c47e3b5ec
rohanparikh <parikhrs@mail.uc.edu>
2023-07-10 23:10:29 +0200

    Basic Working Code

---
+++ src/CtoF.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CtoF {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        double tempC = 0;
+        double tempF = 0;
+
+        String trash = "";
+        boolean done = false;
+
+        do {
+            System.out.print("Enter the temperature in Celsius: ");
+
+            if (in.hasNextDouble()) {
+                tempC = in.nextDouble();
+                in.nextLine();
+                done = true;
+            } else {
+                trash = in.nextLine();
+                System.out.println("\nError: You said the temperature in Celsius was: " + trash);
+                System.out.println("You have to enter a valid amount!\n");
+            }
+        }
+        while (!done);
+
+        tempF = tempC * 9 / 5 + 32;
+
+        System.out.println("The temperature in Fahrenheit is " + tempF + " degrees");
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,116 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        // Part A
+
+        // Task 1
+
+        /*
+        for (int i = 0; i <= 30; i++) {
+            System.out.printf("%3d |", i);
+        }
+         */
+
+
+        // Task 2
+
+        /*
+        for (int i = 30; i >= 0; i--) {
+            System.out.printf("%3d |", i);
+        }
+         */
+
+
+        // Task 3
+
+        /*
+        for (int i = 0; i <= 18; i+=3) {
+            System.out.printf("%3d |", i);
+        }
+         */
+
+
+        // Task 4
+
+        /*
+        for (int i = 10; i >= 0; i-=2) {
+            System.out.printf("%3d |", i);
+        }
+         */
+
+
+        // Part B
+
+        // Task 5
+
+        /*
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        */
+
+
+        // Task 6
+
+        /*
+        for (int row = 5; row > 0; row--) {
+            for (int col = 0; col < row; col++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        */
+
+
+        // Task 7
+
+        /*
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 0; col < 5; col++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        */
+
+
+        // Extra Credit
+
+        /*
+        for (int i = 0; i < 18; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+        for (int i = 0; i < 2; i++) {
+            System.out.print("*");
+            for (int j = 0; j < 16; j++) {
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            System.out.println("");
+        }
+        for (int i = 0; i < 18; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+        for (int i = 0; i < 8; i++) {
+            if (i == 0) {
+                System.out.println("* *");
+            }
+            System.out.print("*");
+            for (int j = 0; j <= i; j++) {
+                System.out.print("  ");
+            }
+            System.out.print("*");
+            System.out.println("");
+        }
+         */
+
+    }
+}
+
